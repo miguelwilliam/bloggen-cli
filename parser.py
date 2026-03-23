29084dc51213035950d34e782cf2038f8e185cef
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-03-22 23:32:04 -0300

    terminei lógica básica do arquivo do parser

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import re
+import datetime
 
-class Regex():
+class Parser():
     @staticmethod
     def getParagraphs(text:str):
         return re.findall(r".+(?:\n(?!\n).+)*", text, flags=re.MULTILINE) # orra foi tempo pra pensar nesse regex, sou mtt burro
@@ -9,26 +10,42 @@ class Regex():
     def formatParagraphs(paragraphs:list):
         formatted_paragraphs = []
         for p in paragraphs:
-            #linhas = p.splitlines()
-            p = f'''<div class="paragraph">
-    <p>{p}</p>
-</div>
-'''
-            formatted_paragraphs.append(p)
+            linhas = p.splitlines()
+            
+            linha_format = ''
+            for linha in linhas:
+                linha_format += f'        <p>{linha}</p>\n'
+            linha_limpa = linha_format.rstrip('\n')
+            
+            formatted_paragraph = (
+                '    <div class="paragraph">\n'
+                f"{linha_limpa}\n"
+                "    </div>\n"
+            )
+            
+            formatted_paragraphs.append(formatted_paragraph)
         return formatted_paragraphs
     
     @staticmethod
-    def save(text:str, file_name:str='blog.html'):
-        with open(f'tests/{file_name}', 'w', encoding='utf-8') as file:
-            file.write(text)
-    
+    def save(blog_body:str, blog_title:str = f'BLOG {datetime.datetime.now().strftime('%d/%m/%Y')}', blog_date = f'{datetime.datetime.now().strftime('%d/%m/%Y')}', file_name:str='blog.html'):
+        with open(f'tests/blog_base.html', 'r', encoding='utf-8') as file:
+            blog_base = file.read()
+        
+        new_blog = re.sub(r'TÍTULO DO BLOG', blog_title, blog_base)
+        new_blog = re.sub(r'DD\/MM\/YYYY', blog_date, new_blog)
+        new_blog = re.sub(r'{{TEXTO}}', blog_body, new_blog)
 
-with open('tests/texto1.txt', 'r', encoding='utf-8') as f:
+        with open(f'tests/{file_name}', 'w', encoding='utf-8') as file:
+            file.write(new_blog)
+
+# USANDO:
+    
+with open('tests/texto2.txt', 'r', encoding='utf-8') as f:
     texto = f.read()
 
-print(f'TEXTO: \n{texto}')
+#print(f'TEXTO: \n{texto}')
 
-paragrafos = Regex.getParagraphs(texto)
-f_par = Regex.formatParagraphs(paragrafos)
+paragrafos = Parser.getParagraphs(texto)
+f_par = Parser.formatParagraphs(paragrafos)
 
-Regex.save('\n'.join(f_par))
+Parser.save('\n'.join(f_par), blog_title='Blog sobre gerar blogs', file_name='blog.html')
